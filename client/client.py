ac8ba15ab7fb02206c15a19747b8892490836d25
Wojciech Wojcik <wojtek.wroclaw@hotmail.com>
2019-10-25 15:59:12 +0200

    server skieleton

--- client/client.py
+++ client/client.py
@@ -1,18 +1,21 @@
 import socket
 
-TCP_IP = '127.0.0.1'
-TCP_PORT = 6666
-BUFFER_SIZE = 1
-MESSAGE = "hello server\n"
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE.encode())
-while True:
-    data = s.recv(1)
-    if data:
-        print("received data:", data.decode('utf-8'))
-    data = ''
+class Client:
+    def __init__(self, master_server_ip, master_server_port, buffer_size):
+        self.buffer_size = buffer_size
+        self.master_server_ip = master_server_ip
+        self.master_server_port = master_server_port
+        self.message = input('Message: ')
 
-s.close()
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((self.master_server_ip, self.master_server_port))
+        s.send(self.message.encode('utf-8'))
+        data = s.recv(self.buffer_size)
+        if data:
+            print("received data:", data.decode('utf-8'))
+        else:
+            print("no data")
+        s.close()
+
 
---
+++ server/server.py
@@ -0,0 +1,37 @@
+import socket
+import threading
+
+
+class MasterServer:
+    def __init__(self, master_server_ip, master_server_port, buffer_size):
+        self.buffer_size = buffer_size
+        self.master_server_ip = master_server_ip
+        self.master_server_port = master_server_port
+        self.next_id = 0
+        self.connections = {}
+        self.addresses = {}
+        threading.Thread(target=self.master_server_loop, args=()).start()
+
+    def client_loop(self, client_id, connection, address):
+        print(connection)
+        print(address)
+        while True:
+            data = connection.recv(self.buffer_size)
+            if not data:
+                break
+            print("received data at ", client_id, ":", data.decode(encoding='utf-8'))
+            connection.send(data)  # echo
+        connection.close()
+
+    def master_server_loop(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((self.master_server_ip, self.master_server_port))
+        s.listen(self.buffer_size)
+        print("Server Started")
+        while True:
+            conn, address = s.accept()
+            self.addresses.update({self.next_id: address})
+            self.connections.update({self.next_id: conn})
+            print('Connection address:', self.next_id, ' : ', address)
+            threading.Thread(target=self.client_loop, args=(self.next_id,conn, address)).start()
+            self.next_id = self.next_id + 1
